f4156b07385fd45096217297961cd18a34a967eb
delberttt <34165068+delberttt@users.noreply.github.com>
2018-10-12 23:54:11 +0800

    Reconfigured spvapp and spvclient
    spvapp handles creating transactions and checking balance. trustedServer onlt provides updated miners list

--- KDCoin/spvClient.py
+++ KDCoin/spvClient.py
@@ -1,43 +1,53 @@
-<<<<<<< HEAD
 from KDCoin.transaction import Transaction
 from KDCoin.keyPair import GenerateKeyPair
+import requests
+import json
 
 # Single Client
 # Functionalities: i) Create Transactions, ii) Verify transactions validated
 
 receiver_private_key, receiver_public_key = GenerateKeyPair()
-print(type(receiver_public_key))
 
 class SPVClient:
 
-    def __init__(self):
-        self.privatekey, self.publickey = GenerateKeyPair()
+    def __init__(self, privatekey, publickey):
+        self.privatekey = privatekey
+        self.publickey = publickey
+
 
 #Creates transaction and sign with private key
     def createTransaction(self, amount, comment):
         transaction_tobemade = Transaction(self.publickey, receiver_public_key,
-                                                   amount, comment)
+                                                   amount, comment, self.privatekey)
+        print(transaction_tobemade.data)
         transaction_tobemade.sign(self.privatekey)
-        print (transaction_tobemade.data)
 
-        return transaction_tobemade
+        if (transaction_tobemade.validate()):
+            print('got into here')
 
+        # json_tobesent = transaction_tobemade.to_json(transaction_tobemade.data)
+            json_tobesent = transaction_tobemade.__str__()
 
-# if __name__ == "__main__":
-#     newclient = SPVClient()
-#     newclient.createTransaction(150,'')
-=======
-class SPVClient:
-    def __init__(self):
-        self.pub_key = ""
-        self.priv_key = ""
+            #Send json to server
+            t1 = requests.post('http://127.0.0.1:8081/createTransaction',
+                               headers = {'Content-Type': 'application/json'},
+                               json = json_tobesent)
+            return True
+        else:
+            return False
 
+#Check acc balance of specified spvclient
+    def checkBalance(self):
+        #Object type 'VerifyingKey' is not JSON serializable
+        publickeyinfo = {'publickey_tobechecked': str(self.publickey)}
+        json_tobesent = json.dumps(publickeyinfo)
 
+        requests.get('http://127.0.0.1:8081/clientCheckBalance',
+                     headers = {'Content-Type': 'application/json'}, json = json_tobesent)
 
-
-
-
-
-if __name__ == '__main__':
-    pass
->>>>>>> master
+sender_privatekey , sender_publickey = GenerateKeyPair()
+newclient = SPVClient(sender_privatekey , sender_publickey)
+#Send Transaction
+newclient.createTransaction(100,'demo')
+#Check Balance
+newclient.checkBalance()
--- KDCoin/trustedServer.py
+++ KDCoin/trustedServer.py
@@ -19,50 +19,11 @@ def addNewMiner(miner):
         miners_list.append(miner)
     return setupServer()
 
-#When Transaction created, Broadcast to all miners done here
-@app.route('/createTransaction', methods = ['POST'])
-def createTransaction():
+@app.route('/updateSPVMinerList')
+def updateMinerList():
+    json_miners_list = json.dumps({'miners_list' : miners_list})
+    return json_miners_list
 
-    if request.headers['Content-Type'] == 'application/json':
-        #Receive data regarding transaction
-        json_received = request.json
-        transaction_data = json.loads(json_received)
-
-        #Now redundant because cannot recreate transaction object
-        sender_publickey = transaction_data['Sender']
-        receiver_publickey = transaction_data['Receiver']
-        amount = transaction_data['Amount']
-        comment = transaction_data['Comment']
-
-
-        #Here is broadcasting
-        #Note that DICT is being appended to miners' tx_list
-        for miner in miners_list:
-            miner.tx_list.append(transaction_data)
-
-        return 'succesfully broadcasted transaction'
-
-    else:
-        return 'wrong format of transaction sent'
-
-#Check with any miner on acc balance (Based on public key received)
-@app.route('/clientCheckBalance', methods = ['GET'])
-def clientCheckBalance():
-
-    if request.headers['Content-Type'] == 'application/json':
-        json_received = request.json
-        publickey_data = json.loads(json_received)
-        publickey_tobechecked = publickey_data['publickey_tobechecked']
-
-        #Check with any miner, request for blockchain state
-        blockchain_state = miners_list[0].blockchain.state
-        clientbalance = blockchain_state[publickey_tobechecked]
-
-        print ('client balance retrieved = ' + str(clientbalance))
-        return clientbalance
-
-    else:
-        return 'wrong format of transaction sent'
 
 
 if __name__ == '__main__':
