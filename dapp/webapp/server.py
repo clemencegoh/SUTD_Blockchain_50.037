b09e31ebf779ff08c45f66c02392f7d386397c92
Clemence Goh <clemencegoh@users.noreply.github.com>
2018-12-02 12:43:00 +0800

    minor edit to server.py - rename contract

--- dapp/webapp/server.py
+++ dapp/webapp/server.py
@@ -14,22 +14,24 @@ with open(contract_source_code_file, 'r') as file:
     contract_source_code = file.read()
 
 contract_compiled = compile_source(contract_source_code)
-contract_interface = contract_compiled['<stdin>:Lottery']
-Lottery = w3.eth.contract(abi=contract_interface['abi'], 
+contract_interface = contract_compiled['<stdin>:FlightInsurance']
+FlightInsurance = w3.eth.contract(abi=contract_interface['abi'],
                           bytecode=contract_interface['bin'])
 
 w3.personal.unlockAccount(w3.eth.accounts[0], '')
-tx_hash = Lottery.constructor().transact({'from':w3.eth.accounts[0]})
+tx_hash = FlightInsurance.constructor().transact({'from':w3.eth.accounts[0]})
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 
 # Contract Object
-lottery = w3.eth.contract(address=tx_receipt.contractAddress, abi=contract_interface['abi'])
+insurance_contract = w3.eth.contract(address=tx_receipt.contractAddress, abi=contract_interface['abi'])
 
 # Web service initialization
 @app.route('/')
 @app.route('/index')
 def hello():
-    return render_template('template.html', contractAddress = lottery.address.lower(), contractABI = json.dumps(contract_interface['abi']))
+    return render_template('template.html', contractAddress = insurance_contract.address.lower(),
+                           contractABI = json.dumps(contract_interface['abi']))
+
 
 if __name__ == '__main__':
     app.run()
