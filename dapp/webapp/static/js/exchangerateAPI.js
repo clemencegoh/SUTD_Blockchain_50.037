400d2f597bbc999da939f03b576d56433c2f5208
delberttt <34165068+delberttt@users.noreply.github.com>
2018-12-07 15:43:40 +0800

    fix exchangerateAPI

--- dapp/webapp/static/js/exchangerateAPI.js
+++ dapp/webapp/static/js/exchangerateAPI.js
@@ -3,15 +3,16 @@ function exchangerateAPI(currencyID,amount){
 
     const url = "https://rest.coinapi.io/v1/exchangerate/ETH/" + currencyID
 
+    // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    // var json = require("json");    
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET",url,false);
     xhttp.setRequestHeader('X-CoinAPI-Key','AABB3709-1E0A-488C-B324-B84C21FB004D');
     xhttp.send();
 
     // Action to be performed when the document is read;
-    // var response = xhttp.responseJSON;
-    // console.log(response)
     var response = xhttp.responseText;
+    // console.log(response)
     var json_response = JSON.parse(response)
     var rate = json_response['rate']
     var currencytoeth = 1/rate
